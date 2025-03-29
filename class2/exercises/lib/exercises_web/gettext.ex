3355f5e2113d2801419034257098b8285bb27831
LKlemens <klemens.lukaszczyk@gmail.com>
2025-03-29 19:45:18 +0100

    update deps

--- class2/exercises/lib/exercises_web/gettext.ex
+++ class2/exercises/lib/exercises_web/gettext.ex
@@ -20,5 +20,5 @@ defmodule ExercisesWeb.Gettext do
 
   See the [Gettext Docs](https://hexdocs.pm/gettext) for detailed usage.
   """
-  use Gettext, otp_app: :exercises
+  use Gettext.Backend, otp_app: :exercises
 end
